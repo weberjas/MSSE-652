7e6b7294f2d332aac59dd13cc77c579939cbb7d3
Jason Weber <weberjas@gmail.com>
2015-03-02 09:59:46 -0700

    Initial Commit

---
+++ MSSE 652/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSSE 652
+//
+//  Created by Jason Weber on 3/2/15.
+//  Copyright (c) 2015 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MSSE 652/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MSSE 652
+//
+//  Created by Jason Weber on 3/2/15.
+//  Copyright (c) 2015 msse652. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
