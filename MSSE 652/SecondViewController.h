fe84309103bc37d9f2c2ce1ee3606a124a24eba8
Jason Weber <weberjas@gmail.com>
2015-03-03 14:30:30 -0700

    Laid out ui and created view controllers

---
+++ MSSE 652/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MSSE 652
+//
+//  Created by Jason Weber on 3/2/15.
+//  Copyright (c) 2015 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
