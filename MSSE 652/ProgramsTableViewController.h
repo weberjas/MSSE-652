33e017ddd0709f20a4dfb2ecc328a039656a8607
Jason Weber <weberjas@gmail.com>
2015-03-13 16:11:12 -0600

    Successfully implemented the web service client which reads from the Regis program service.

---
+++ MSSE 652/ProgramsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProgramsTableViewController.h
+//  MSSE 652
+//
+//  Created by Jason Weber on 3/9/15.
+//  Copyright (c) 2015 msse652. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgramsTableViewController : UITableViewController
+
+@end
